35367fc418b943da119f1fe1c8e0c6f4673d12fb
Lucas 'lutaw' Barbosa <lucasbarbosa81612@gmail.com>
2026-04-09 17:10:52 -0300

    refatoração do ReportCard pra o padrão mikan fofinha de qualidade kkk

--- src/components/ReportCard.jsx
+++ src/components/ReportCard.jsx
@@ -5,62 +5,59 @@ function ReportCard() {
 
     return(
     <section>
-        <div class="Report_card">
+        <div className="Report_card">
     <header>
-        <h1>Meu nome</h1>
+        <h2>Meu nome</h2>
+        <figure>
+            <img src="" alt="Hope fragments" />
+        </figure>
         <nav>
             <ul>
-                <li><a href="index.html" class="ativo">Home</a></li>
-                <li><a href="pages/galeria.html">Galeria de artes fofas</a></li>
-                <li><a href="pages/animes.html">Meus animes</a></li>
-                <li><a href="pages/jogos.html">Meus jogos</a></li>
+                <li><a href="index.html" className="ativo">1</a></li>
+                <li><a href="pages/galeria.html">2</a></li>
+                <li><a href="pages/animes.html">3</a></li>
+                <li><a href="pages/jogos.html">4</a></li>
+                <li><a href="index.html" class="ativo">5</a></li>
+                <li><a href="pages/galeria.html">6</a></li>
+                <li><figure><img src="" alt="Hope fragments funfacts" /></figure></li>
             </ul>
         </nav>
     </header>
 
-    <div className="minhas_contas">
-    <h2>STEAM</h2><a href="">STEAM</a>
-    <h2>Xbox</h2><a href="">XBOX</a>
-    <h2>MyAnimeList</h2><a href="">MyAnimeList</a>
-    <h2>Discord</h2><a href="">DISCORD?</a>
+    <div className="status_fisicos"> 
+    <h2>Height</h2><a>5'4"</a>
+    <h2>Chest</h2><a>35 in</a>
+    <h2>Weight</h2><a>125 lbs</a>
+    <h2>Blood Type</h2><a>A</a>
     </div>
 
-    <div>
-    <h2>Breve resumo de mim</h2>
-    <p>ILLYA FAN, NEKOPARA LOVER, PRONOUNS: FATE/STAY NIGHT</p>
-
     <div className="gosto">
-    <h2>gosto</h2>
+    <h2>Date of birth</h2>
+    <p>May 12th</p>
+
+    <h2>Likes</h2>
     <ul>
-        <li>exemplo 1, exemplo 2, exemplo 3</li>
+        <li>Bathrooms smells, alcohol</li>
     </ul>
     </div>
 
     <div className="não_gosto">
-    <h2>não gosto</h2>
+    <h2>Dislikes</h2>
     <ul>
-        <li>exemplo 1, exemplo 2, exemplo 3</li>
+        <li>Oversized Things</li>
     </ul>
     </div>
 
     <div className="SLA">
-    <h2>aniversario</h2>
-    <p>04/04</p>
-
-    <h2>anime favorito</h2>
-    <p>TATE NO YUSHA</p>
-
-    <h2>jogo favorito</h2>
-    <p>MINEIRINHO ULTRA ADVENTURES</p>
+    <h2>Special Notes</h2>
+    <p>Ultimate Nurse</p>
     </div>
 
     <figure>
         <img src="src/assets/download.jpg" alt="" />
     </figure>
     </div>
-
-    </div>
-
+    
     </section>)
 }
 
