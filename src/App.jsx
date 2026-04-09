b34deb36fdb375a38221f3531d8ccabd42842c02
Lucas 'lutaw' Barbosa <lucasbarbosa81612@gmail.com>
2026-04-09 02:33:57 -0300

    Commit inicial: Css da pagina 'Sobre mim' reaproveitada e rascunho do html no App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className="Report_card">
+      <h1>Máquina de Gacha da Esperança</h1>
+      <p> Aqui está o mais novo produto da monokuma interprises! O GACHARONPA (ou sera que dangagacha puhuhu)!</p>
+
+      <section>
+      <h2>PARABÉNS! Seu prêmio foi: </h2>
+      <figure>
+        <img src="https://camo.githubusercontent.com/c929b81dedb620fec60d7f4aac27eb028e99343fbf0a56dc4ec088d075fe0e10/68747470733a2f2f6d656469612e74656e6f722e636f6d2f42413954743872724a376341414141692f6d696b616e2d6d696b616e2d7473756d696b692e676966" alt="Gacha Card" />
+        <figcaption>
+          <p>Não há garantia... A única garantia é o despair HAHAHAH!</p>
+        </figcaption>
+      </figure>
+      </section>
+
+
+      <button>PULL (100 Monocoins)</button>
+    </div>
+  )
+}
+
+export default App
